fa08f4fc2a6b923725e3d3f0a353d70aa97006c4
Taha Ahmed <110302670+Taha-Ahmed99@users.noreply.github.com>
2022-08-18 21:45:26 +0500

    Add files via upload

---
+++ Car_Rental_Web/Models/AcUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Car_Rental_Web.Models
+{
+    public class AcUser
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
